fe29efe5b166749f7673cca6dfc3f48c772bff85
moezthemaster <you@example.com>
2022-10-08 17:19:20 +0000

    edit dockerfile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>version 3</p>
+        <p>version 1.0.0</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
